c0d325fcc1ffa91b55373ab1096ed3302330b263
Jasmine Akbari <jasmine_akbari@amesite.com>
2021-12-13 11:59:16 -0500

    working POC

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
+require('dotenv').config();
 const sgMail = require('@sendgrid/mail')
 
 // API REQUEST
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-const msg = {
-  to: 'jasmine_akbari+01@amesite.com', 
-  from: 'jasmine_akbari@amesite.com', 
-  subject: 'First Send Grid Email',
-  text: 'This is a test email',
-  html: '<strong>This is a test email</strong>',
-}
+function getMessage(data) {
 
-sgMail
-  .send(msg)
-  .then((response) => {
-    console.log(response[0].statusCode)
-    console.log(response[0].headers)
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+    return {
+        from: {
+            email: data.from,
+            name: data.fromname
+        },
+        to: data.to,
+        subject: data.subject,
+        text: data.text,
+        html: `<strong>${data.text}</strong>`,
+    };
+}
+  
+  async function sendEmail() {
+    const contact_us_email = process.env.CONTACT_US_EMAIL;
+
+    const email_data = {
+        from: contact_us_email,
+        fromname: 'Amesite',
+        to: 'jasmine_akbari@amesite.com',
+        subject: 'Test email with Node.js and SendGrid',
+        text: 'This is a test email using SendGrid from Node.js'
+    }
+
+    try {
+        await sgMail.send(getMessage(email_data));
+        console.log('Test email sent successfully');
+    } catch (error) {
+        console.error('Error sending test email');
+        console.error(error);
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    }
+  }
+
+
+
+(async () => {
+    console.log('Sending test email');
+    await sendEmail();
+})();
